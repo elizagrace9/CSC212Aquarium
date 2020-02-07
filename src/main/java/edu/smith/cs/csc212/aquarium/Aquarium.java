9c5cf655d3550e80be28490d342b719626c1fede
ElizaGrace <ElizaGrace@131.229.47.35>
2020-02-07 14:46:34 -0500

    Lab #1 -- fish class started.

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -43,20 +43,31 @@ public class Aquarium extends GFX {
 
 	int fish1X = getWidth() + 100;
 	int fish2X = getWidth() + 300;
+	//A small red fish
+	int fish3X = -100;
 
+	Fish nemo = new Fish(Color.magenta, 
+			250, 250, true);
+	Fish marlin = new Fish(Color.green, 
+			100,100, false);
+	
 	@Override
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, getWidth(), getHeight());
-
+		
+		nemo.draw(g);
+		marlin.draw(g);
+		
+		
 		// Draw the fish!
 		DrawFish.facingLeft(g, Color.yellow, fish1X, 200);
 		// Draw the confused fish!
 		DrawFish.facingRight(g, Color.green, fish2X, 300);
 
 		// What if we wanted this little fish to swim, too?
-		DrawFish.smallFacingLeft(g, Color.red, 200, 100);
+		DrawFish.smallFacingLeft(g, Color.red, fish3X, 100);
 
 		// Draw our snail!
 		algorithm.draw(g);
@@ -64,7 +75,16 @@ public class Aquarium extends GFX {
 		// Move the fish!
 		fish1X -= 1;
 		fish2X -= 2;
-	}
+		fish3X += 4;
+		
+		//Little red fish moves off screen without disappearing first. 
+		//Describe this better when you submit.
+		if (fish3X > getWidth() +100) {
+			fish3X = -100;
+		}
+		}
+	
+	
 
 	public static void main(String[] args) {
 		// Uncomment this to make it go slower!
@@ -76,5 +96,4 @@ public class Aquarium extends GFX {
 		GFX app = new Aquarium();
 		app.start();
 	}
-
 }
---
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -0,0 +1,49 @@
+package edu.smith.cs.csc212.aquarium;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Fish {
+		//Every fish has an x which is an int
+		int x;
+		int y;
+		Color color;
+		boolean isLittle;
+		boolean facingLeft;
+		//Every fish has a destination
+		int destX;
+		int destY;
+		
+		
+		public Fish(Color c, int startX, 
+				int startY, boolean isLittle) {
+			this.color = c;
+			this.x = startX;
+			this.y = startY;
+			this.isLittle = isLittle;
+			
+			this.destX = 450;
+			this.destY = 450;
+		}
+		
+		public void swim() {
+			if (this.y < destY) {
+			this.y += 1;
+			}
+		}
+			
+		
+		public void draw(Graphics2D g) {
+			this.swim();
+			//Assumption that fish face left and are small
+			//facingRight, facingLeft, and facingRight
+			DrawFish.smallFacingLeft(g, 
+					this.color, this.x, this.y);
+			
+		//if statements go here
+			
+			
+		}
+			
+		}
+}
